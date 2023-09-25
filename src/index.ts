b7738caf1e4124689b63674ed23eb0fe72bcd7b4
공상헌 <boipqiod@naver.com>
2023-09-25 19:27:54 +0900

    Delete. 사진 삭제 Update. 일부 내용 수정

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
-import PageController from "./controller/PageController";
-
 import './assets/styles/common.css';
 import './assets/styles/loading.css';
 import './assets/styles/main.css';
-import './assets/styles/animate.css';
+import './assets/styles/skills.css';
+import './assets/styles/career.css';
 import './assets/styles/project.css';
 import './assets/styles/contact.css';
-
-
-addEventListener('load', PageController.instance.init);
